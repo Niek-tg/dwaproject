d4788b94e6d796c1a8c89ee2b5c671bc9ac8def5
Dick Pino <dickpino@hotmail.com>
2015-11-18 15:03:06 +0100

    config.js

---
+++ spa-framework-master/config.js
@@ -0,0 +1,14 @@
+/**
+ * Created by dickpino on 12-11-15.
+ */
+module.exports = {
+    rethinkdb: {
+        host: 'localhost',
+        port: 28015,
+        authKey: '',
+        db: 'protodb'
+    },
+    express: {
+        port: 3000
+    }
+};
