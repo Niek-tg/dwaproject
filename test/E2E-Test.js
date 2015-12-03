1795ac2dccee2f8c52ebc21579420a181cbbeb8a
TjeuAFCA <tjeujansen@hotmail.com>
2015-12-03 12:31:50 +0100

    fixed test for opening and undoing memory models

--- test/E2E-Test.js
+++ test/E2E-Test.js
@@ -1,26 +1,26 @@
 /*
-* E2E User story 1
-* */
+ * E2E User story 1
+ * */
 //
 //var webdriverio = require('webdriverio');
 //var expect = require('chai').expect;
 
 
 /*
-* Situation: database connection bestaat
-*            RethinkDB database draait
-*            Server percolator draait
-*            Webrowser wordt geopend
-*            Er wordt een lijst getoond met geheugenmodellen
-*            Geheugenmodellen voldoen aan juiste aantal
-* When:
-*            Geheugenmodel geselecteerd
-*
-* Then:
-*            Gelecteerde geheugenmodel wordt getoond
-*            Geheugenmodel is het zelfde als in de database
-*
-* */
+ * Situation: database connection bestaat
+ *            RethinkDB database draait
+ *            Server percolator draait
+ *            Webrowser wordt geopend
+ *            Er wordt een lijst getoond met geheugenmodellen
+ *            Geheugenmodellen voldoen aan juiste aantal
+ * When:
+ *            Geheugenmodel geselecteerd
+ *
+ * Then:
+ *            Gelecteerde geheugenmodel wordt getoond
+ *            Geheugenmodel is het zelfde als in de database
+ *
+ * */
 
 
 
@@ -28,22 +28,22 @@
 var webdriverio = require('webdriverio');
 var expect = require('chai').expect;
 
-describe("E2E test get homepage", function(){
+describe("E2E test get homepage", function () {
 
     this.timeout(20000);
     var browser;
 
-    before(function (done){
+    before(function (done) {
         browser = webdriverio.remote({
             desiredCapabilities: {
-                browserName: 'firefox'
+                browserName: 'chrome'
             }
         });
         browser.init(done)
     });
 
 
-    it("Should retrieve memorymodel information", function(done) {
+    it("Should retrieve memorymodel information", function (done) {
         browser
             .url("http://localhost:3000")
             .waitForExist("#92524038-f0e2-4db2-ad01-321a9040df02", 2000)
@@ -63,36 +63,31 @@ describe("E2E test get homepage", function(){
             });
     });
 
-    it("Should undo action", function(done) {
+    it("Should undo action", function (done) {
         browser
-            .url("http://localhost:3000")
-            .waitForExist("#92524038-f0e2-4db2-ad01-321a9040df02", 2000)
-
-            .element('#92524038-f0e2-4db2-ad01-321a9040df02')
+            .waitForExist("#undoButton", 2000)
+            .element("#undoButton")
             .click().then(function (result) {
                 browser
-                    .waitForExist("#undoButton", 2000)
-                    .element("#undoButton")
-                    .click().then(function (result) {
-                        browser
-                            .waitForValue("#owner", 2000)
-                            .getText("#headerTitle h5").then(function (result) {
-                                console.log(result[0]);
-                                console.log(result[1]);
-                                console.log(result[2]);
-                                setTimeout(function() {
-                                    expect(result[0]).to.equal("Owner: Jack");
-                                    expect(result[1]).to.equal("Modelname: Mijn geheugenmodel");
-                                    expect(result[2]).to.equal("Version: 1");
-                                    done();
-                                }, 5000);
-                            });
-                        });
+                    .waitForValue("#owner", 2000)
+                    .getText("#headerTitle h5").then(function (result) {
+                        console.log("Should undo action");
+                        console.log(result[0]);
+                        console.log(result[1]);
+                        console.log(result[2]);
+
+                        expect(result[0]).to.equal("Owner: Jack");
+                        expect(result[1]).to.equal("Modelname: Mijn geheugenmodel");
+                        expect(result[2]).to.equal("Version: 1");
+                        done();
+
                     });
             });
+    });
 
-    after(function(done){
+    after(function (done) {
         browser.end(done);
     });
 
-});
+})
+;
