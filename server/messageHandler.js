2d9f63112df65f7b5f674db76a5b4a3b547e0c0a
Thomas van der Zee <thomasvdzee@gmail.com>
2015-12-08 10:25:48 +0100

    Converted routes to websocket cases in messageHandler

--- server/messageHandler.js
+++ server/messageHandler.js
@@ -15,13 +15,28 @@ messageHandler.identifyMessage = function(message, websocket){
     switch(message.msgType){
 
         case "subscribeToChanges":
-            messageHandler.subscribeToChanges(message,websocket);
+            messageHandler.subscribeToChanges(message, websocket);
+            break;
+
+        case "getAllModels":
+            messageHandler.getAllMemoryModels(message, websocket);
+            break;
+
+        case "getModelById":
+            messageHandler.getModelById(message, websocket);
+            break;
+
+        case "makeNewModel":
+            messageHandler.makeNewModel(message, websocket);
+            break;
+
+        case "deleteModel":
+            messageHandler.deleteModel(message, websocket);
             break;
 
         default :
             // TODO come up with a default action
             break;
-
     }
 };
 
@@ -38,6 +53,81 @@ messageHandler.subscribeToChanges = function(message, websocket){
                 websocket.send(JSON.stringify({msgType :"newData",data:row}))
             }
         );
+    });
+};
+
+messageHandler.getAllMemoryModels = function(message, websocket){
+    queries.getAll(function (err, result) {
+        if(err)
+            return websocket.send(JSON.stringify({msgType: "errorMsg", data:err}));
+
+        if (!result)
+            return websocket.send(JSON.stringify({msgType: "errorMsg", data: "Something went wrong in the getAll query: No memory models were found!"}));
+
+        var resultsArray = [];
+
+        result.forEach(function (r) {
+            var inList = false;
+            var i = 0;
+            resultsArray.forEach(function (result) {
+                if (r.mmid === result.mmid) {
+                    inList = true;
+                    if (r.version > result.version) {
+                        resultsArray[i] = r;
+                    }
+                }
+                else inList = false;
+                i++;
+            });
+            if (inList === false) {
+                resultsArray.push(r);
+            }
+        });
+        websocket.send(JSON.stringify({msgType: "getAllModels", data: resultsArray}));
+    });
+};
+
+messageHandler.getModelById = function(message, websocket){
+    var mmid = message.data.id;
+    var version = (message.data.version) ? parseInt(message.data.version) : null;
+
+    if (mmid) {
+        var cb = function (err, result) {
+            if (err)
+               return websocket.send(JSON.stringify({msgType:"errorMsg",data:err}));
+
+            if (result[0])
+               return websocket.send(JSON.stringify({msgType:"getModelById", data: result[0]}));
+
+            return websocket.send(JSON.stringify({msgType:"errorMsg", data:"Something went wrong in the getModelById callback: ID or version does not exist"}));
+        };
+
+        if (version) queries.getMemoryModelByIdAndVersion(mmid, version, cb);
+        else query = queries.getMemoryModelById(mmid, cb);
+
+    } else return websocket.send(JSON.stringify({msgType:"errorMsg", data:"Something went wrong in getModelById query: not a valid id"}));
+};
+
+messageHandler.makeNewModel = function(message, websocket){
+    var language = message.data.language;
+    var owner = message.data.owner;
+    var modelName = message.data.modelName;
+
+    queries.createNewMemoryModel({language: language, owner: owner, modelName: modelName},function(err, result){
+        if(err) websocket.send(JSON.stringify({msgType:"errorMsg", data:"Something went wrong in query createNewMemorymodel " + err}));
+
+       websocket.send(JSON.stringify({msgType:"makeNewModel", data: result}));
+    });
+};
+
+messageHandler.deleteModel = function(message, websocket){
+    var mmid = message.data.id;
+    var version = parseInt(message.data.version);
+    queries.deleteLatestversion(mmid, version, function (err, result) {
+        if (err)
+            return websocket.send(JSON.stringify({msgType:"errorMsg", data: "Something went wrong in the delete query, unexpected error: " +err}));
+
+        return websocket.send(JSON.stringify({msgType:"deleteModel", data: "Delete request completed"}));
     });
 };
 
