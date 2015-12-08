b8d9950591598fb29a6db0e7ae7bd86b6eb8ea48
Thomas van der Zee <thomasvdzee@gmail.com>
2015-12-08 09:44:34 +0100

    Added documentation to messagehandler

--- server/messageHandler.js
+++ server/messageHandler.js
@@ -1,8 +1,15 @@
 var queries = require('./queries/queries.js');
 
+/**
+ * Exportable object that stores all methods of the messageHandler
+ */
 var messageHandler = {};
 
-
+/**
+ * Handles all incoming messages from clients and calls the corresponding methods
+ * @param message Message that has been received from client
+ * @param websocket Connection to the websocket so a new message can be sent to client later
+ */
 messageHandler.identifyMessage = function(message, websocket){
    message = JSON.parse(message);
     switch(message.msgType){
@@ -18,6 +25,11 @@ messageHandler.identifyMessage = function(message, websocket){
     }
 };
 
+/**
+ * Subscribes to a memory model and sends new updates of the model to the client
+ * @param message Message that has been received from client, has a mmid in this case
+ * @param websocket Connection to the websocket so a new message can be sent to client
+ */
 messageHandler.subscribeToChanges = function(message, websocket){
     queries.subscribeToChanges(message.data.mmid, function(err, cursor) {
         cursor.each(
