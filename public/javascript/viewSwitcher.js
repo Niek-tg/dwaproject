8635eb07abb54be1b0fb965b615ef883ff18fd12
Niek te Grootenhuis <N.TeGrootenhuis@student.han.nl>
2015-12-09 10:08:43 +0100

    added documentation to the viewswitcher

--- public/javascript/viewSwitcher.js
+++ public/javascript/viewSwitcher.js
@@ -1,12 +1,26 @@
 
+/**
+ * Holds the name of the current view
+ * @type {string}
+ */
+var currentView = "diagramView";
+
+/**
+ * Switches to the diagram view
+ */
 function enableDiagramView(){
     $('#memoryModel').load('./../views/diagramview.html');
+    currentView = "diagramView";
 }
 
+/**
+ * Switches to the code view
+ */
 function enableCodeView(){
     $('#memoryModel').load('./../views/codeview.html');
+    currentView = "codeView";
 }
 
 $(document).ready(function(){
     enableDiagramView();
-})
+});
