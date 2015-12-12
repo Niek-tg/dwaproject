14e5cd3091e0c1b0aa2be61a18108c0499473ab2
Dick Pino <dickpino@hotmail.com>
2015-12-12 08:09:48 +0100

    Code clean-up

--- public/javascript/JSONEditorModule.js
+++ public/javascript/JSONEditorModule.js
@@ -4,37 +4,24 @@
  */
 
 //var connection = new WebSocket("ws://localhost:3000");
-//
-//connection.onmessage = function (message) {
-//    var data = JSON.parse(message.data);
-//    console.log(data);
-//
-//    switch (data.msgType) {
-//        case "getModelById":
-//            setMemoryModel(data.data);
-//            console.log("komt in switch");
-//            break;
-//
-//    }
-//};
+
 
 /**
  *  Adds a JSON editor on the page and fills it with data selected from the Memorymodel list.
  *  When the "opslaan" button is clicked it will be saved to the database.
+ *  When the "nieuw geheugenmodel" button is clicked it will create a new memorymodel.
  */
-
 
 function initJSONEditor() {
 
     var container = document.getElementById('jsoneditor');
-    var editor = new JSONEditor(container);
     var selectedMemoryModel = currentMemoryModel;
-    editor.set(selectedMemoryModel);
-
+    var editor = new JSONEditor(container, options, selectedMemoryModel );
     var newMemorymodelButton = $('<input/>').attr({type: 'button', id: 'setJSON', value: 'Nieuw geheugenmodel'});
-        $("#JSONButtons").append(newMemorymodelButton);
 
-        $("#setJSON").click(function newMemoryModel() {
+    $("#JSONButtons").append(newMemorymodelButton);
+
+    $("#setJSON").click(function newMemoryModel() {
             var modelInfo = {
                 'language': '',
                 'owner': '',
@@ -95,15 +82,34 @@ function initJSONEditor() {
         $("#JSONButtons").append(saveMemorymodelButton);
 
         $("#getJSON").click(function saveMemoryModel() {
-            //var json = editor.get();
             var newMemoryModel = editor.get();
             sendMessage({msgType:'updateMemoryModel', data:newMemoryModel});
-
-            //alert(JSON.stringify(json, null, 2));
         });
 
 }
 
+/**
+ * Function to disable some fields en values in de JSON editor.
+ * @type {{editable: Function}}
+ */
+
+var options = {
+    editable: function (object) {
+        console.log(object);
+        switch (object.field) {
+            case 'mmid':
+            case 'id':
+                return false;
+            break;
+
+            default:
+                return {
+                    field: false,
+                    value: true
+                };
+        }
+    }
+};
 
 
 
