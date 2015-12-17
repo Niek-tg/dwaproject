7400c69f1d0c02e10b125c31f1cfc944281e01df
Dick Pino <dickpino@hotmail.com>
2015-12-17 10:18:25 +0100

    JSONeditorSocket reviewed

--- public/javascript/JSONEditorSocket.js
+++ public/javascript/JSONEditorSocket.js
@@ -6,36 +6,39 @@
 var jsonEditorConnection = new WebSocket("ws://localhost:3000");
 
 /**
- * When jsonEditorConnection is opened a message is send to the server to identify wich socket it is en the state of it
+ * When jsonEditorConnection is opened a message will be send to the server to identify which socket
+ * it is and the state of it
  */
 
 jsonEditorConnection.onopen = function () {
     jsonEditorSendMessage({msgType: 'socketIdentifier', identity: 'jsonEditor', state: 'notActive'})
-}
+};
 
 /**
- * Send a percolator message to messageHandler with connection en data
+ * Sends a percolator message to messageHandler with connection and data
  * @param connection Contains the socketconnection
  * @param data Contains the data that needs to be send
  */
+
 jsonEditorSendMessage = function (data) {
     sendMessage(jsonEditorConnection, data);
-}
+};
 
 /**
- * Sends a JSON message true messageHandler client to the server
- * @param data
+ * Receives incoming messages from the server.
+ * @param message which is send by the server.
  */
+
 jsonEditorConnection.onmessage = function (message) {
     messageHandlerClient(message);
-}
+};
 
 /**
- * Triggered when the windows is closed. the current cursor is being unsubscribed to prevent server errors and eventually the websocket is closed
+ * Triggered when the windows is closed. The current cursor is being unsubscribed to prevent server errors and
+ * eventually the websocket is closed
  */
+
 window.onbeforeunload = function () {
-    jsonEditorConnection.onclose = function () {
-    }; // disable onclose handler first
     jsonEditorSendMessage({msgType: "unsubscribeToCurrentCursor"});
     jsonEditorConnection.close()
 };
@@ -45,15 +48,13 @@ window.onbeforeunload = function () {
  */
 
 changeJsonEditorState = function () {
-    console.log("CODE VIEW IS NOT ACTIVE");
     jsonEditorSendMessage({msgType: 'socketIdentifier', identity: 'jsonEditor', state: 'notActive'})
-}
+};
 
 /**
  * Change the state of the json editor to active
  */
 
 activeJsonEditorState = function () {
-    console.log("CODE VIEW IS ACTIVE");
     jsonEditorSendMessage({msgType: 'socketIdentifier', identity: 'jsonEditor', state: 'active'})
-}
+};
