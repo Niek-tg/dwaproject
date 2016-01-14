77d4e07e4d4ddac1273194f40586321ff27174e0
Niek te Grootenhuis <N.TeGrootenhuis@student.han.nl>
2016-01-14 13:42:56 +0100

    removed duplicate code in the changeHandler

--- server/changeHandler.js
+++ server/changeHandler.js
@@ -25,40 +25,6 @@ function checkForChanges(newModel, oldModel){
     return pendingChangeList;
 }
 
-
-module.exports = {
-    checkForChanges : checkForChanges
-};
-
-
-var diff = require('deep-diff').diff;
-
-/**
- *
- * @type {Array}
- */
-var pendingChangeList = [];
-
-/**
- * Returns all the changes between the old and new memory model
- * @param newModel
- * @param oldModel
- * @returns {Array}
- */
-function checkForChanges(newModel, oldModel){
-
-    pendingChangeList = [];
-
-    var diffs = diff(oldModel, newModel);
-    console.log('changes zijn' , diffs);
-    if(diffs) diffs.forEach(function(diff){
-        pendingChangeList.push(diff);
-    });
-
-    return pendingChangeList;
-}
-
-
 module.exports = {
     checkForChanges : checkForChanges
 };
